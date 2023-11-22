4ee5b3a44c8dade1a3626dc064864fcc57c0e673
Iliana Gavrilova <ilianagis98@gmail.com>
2023-11-22 19:16:34 +0300

    extractNumber

--- js/functions.js
+++ js/functions.js
@@ -13,3 +13,17 @@ const isPalindrom = (string) => {
 };
 
 //Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN
+const extractNumber = (string) => {
+  if (typeof string === 'number') {
+    return string;
+  }
+
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      result += string.at(i);
+    }
+  }
+
+  return parseInt(result, 10);
+};

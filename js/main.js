71fac09e3086d23da2bae14aef08216851f1f7e0
Iliana Gavrilova <ilianagis98@gmail.com>
2023-11-29 14:25:31 +0300

    getRandomInteger

--- js/main.js
+++ js/main.js
@@ -23,3 +23,9 @@ const DESCRIPTONS = [
 ];
 const NAMES = ['Мария', 'Александр', 'Алексей', 'Игорь', 'Арина', 'Регина', 'Михаил', 'Виктория'];
 
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};

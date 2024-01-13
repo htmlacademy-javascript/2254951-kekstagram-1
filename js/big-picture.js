42fea22e92c7b287bdb2ab361c26be7d9636dbd9
Iliana Gavrilova <ilianagis98@gmail.com>
2024-01-13 20:52:21 +0300

    fix: внесла правки

--- js/big-picture.js
+++ js/big-picture.js
@@ -4,18 +4,19 @@ const bigPicture = document.querySelector('.big-picture');
 const bigPictureCancel = document.querySelector('.big-picture__cancel');
 const commentsCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
+const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+const bigPictureCommentList = document.querySelector('.social__comments');
 
 const renderPicture = ({ url, likes, description }) => {
   bigPicture.querySelector('.big-picture__img img').src = url;
   bigPicture.querySelector('.likes-count').textContent = likes;
-  // bigPicture.querySelector('.comments-count').textContent = comments;
   bigPicture.querySelector('.social__caption').textContent = description;
 };
 
 const onPopupKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
-    hideBigPicture(); //если ставлю после hideBigPicture, то та же самая ошибка и с onPopupKeydown ('' was used before it was defined)
+    hideBigPicture();
   }
 };
 
@@ -24,15 +25,15 @@ const hideComments = () => {
   commentsLoader.classList.add('hidden');
 };
 
-const hideBigPicture = () => {
+function hideBigPicture () {
   bigPicture.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
 
   document.removeEventListener('keydown', onPopupKeydown);
-};
+}
 
-const showBigPicture = () => {
-  renderPicture();
+const showBigPicture = (picture) => {
+  renderPicture(picture);
 
   bigPicture.classList.remove('hidden');
   document.querySelector('body').classList.add('modal-open');
@@ -43,4 +44,26 @@ const showBigPicture = () => {
   hideComments();
 };
 
-export { showBigPicture };
+const renderComment = ({ avatar, description, message}) => {
+  const newComment = commentTemplate.cloneNode(true);
+
+  newComment.querySelector('.social__picture').src = avatar;
+  newComment.querySelector('.social__text').alt = description;
+  newComment.querySelector('.social__text').textContent = message;
+
+  return newComment;
+};
+
+const renderComments = (comments) => {
+  const fragment = document.createDocumentFragment();
+
+  comments.forEach((comment) => {
+    const newComment = renderComment(comment);
+
+    fragment.append(newComment);
+  });
+
+  bigPictureCommentList.append(fragment);
+};
+
+export { showBigPicture, renderComments };

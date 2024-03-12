defe13f3d00195a62c64cb50e884285f633dcc2d
Iliana Gavrilova <ilianagis98@gmail.com>
2024-03-13 00:17:31 +0300

    feat: вносит правки

--- js/form.js
+++ js/form.js
@@ -1,6 +1,9 @@
 const HASHTAG_COUNT_MAX = 5;
 const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
-const HASHTAG_ERROR_TEXT = 'Неправильно заполнены хэштеги';
+const HASHTAG_ERROR_TEXT = 'Хэш-тег должен начинаться со знака #, ' +
+'содержать только кириллицу или латиницу, ' +
+'длину от 1-19 символов не включая знак #. ' +
+'Максимальное количество хэштегов: 5';
 
 const form = document.querySelector('.img-upload__form');
 const overlay = document.querySelector('.img-upload__overlay');
@@ -11,9 +14,10 @@ const hashtagField = document.querySelector('.text__hashtags');
 const commentField = document.querySelector('.text__description');
 
 const pristine = new Pristine(form, {
-  classTo: 'img-ulpoad__field-wrapper',
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
-  errorTextClass: 'img-upload__field-wrapper__error',
+  errorTextClass: 'img-upload__field-wrapper__error'
 });
 
 const openModal = () => {
@@ -25,7 +29,7 @@ const openModal = () => {
 const closeModal = () => {
   overlay.classList.add('hidden');
   body.classList.remove('modal-open');
-  document.addEventListener('keydown', onDocumentKeydown);
+  document.removeEventListener('keydown', onDocumentKeydown);
 };
 
 const isTextFieldFocused = () => document.activeElement === hashtagField || document.activeElement === commentField;
@@ -46,6 +50,8 @@ const areTagsUnique = (tags) => {
   return lowCaseTags.length === new Set(lowCaseTags).size;
 };
 
+const validateDescription = (value) => value.length <= 140;
+
 const validateTags = (value) => {
   const tags = value
     .trim()
@@ -63,6 +69,8 @@ const onFileInputChange = () => {
 };
 
 pristine.addValidator(hashtagField, validateTags, HASHTAG_ERROR_TEXT);
+
+pristine.addValidator(commentField, validateDescription, 'Длина комментария больше 140 символов');
 
 const onFormSubmit = (evt) => {
   evt.preventDefault();
